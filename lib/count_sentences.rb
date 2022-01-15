3944ed8513a95c1dbe3ce02c8aa4807a8d952499
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-16 09:48:12 +1030

    Used Regex to split string

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    false
   end
 
   def count_sentences
-
+    char_arr = self.split(/[!.?]+/)
+    char_arr.count
   end
 end
